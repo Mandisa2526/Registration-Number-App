6bbfeffa6d3be446dd880688294fb4ce62fc8847
root <root@LAPTOP-DK555H0M>
2023-10-10 13:22:22 +0200

    new tests

--- test/regNum.tests.js
+++ test/regNum.tests.js
@@ -2,6 +2,7 @@ import assert from "assert";
 import pgPromise from 'pg-promise';
 import Query from '../services/queryReg.js';
 import RegistrationNumberFact from "../js/registrationNum.js";
+import { log } from "console";
 
 
 const pgp = pgPromise();
@@ -24,28 +25,55 @@ describe('Registration number factory function code test', function () {
         assert.equal('Registration number exists!' == '', result);
 
     });
+    it("should be able to return error message when the registration number is not entered", async function () {
+        let registrationNumberObject = RegistrationNumberFact();
+
+        registrationNumberObject.addRegistration("CJ 123456");
+        registrationNumberObject.addRegistration("CJ 123456");
+
+        let results = registrationNumberObject.getError();
+
+        assert.equal('Enter registration number!' == '', results);
+    });
 
 });
 describe('the Registration Number App database function/Code', function () {
 
+    beforeEach(async function () {
+        try {
+            this.timeout(10000);
+            // clean the tables before each test run
+            await db.none("TRUNCATE TABLE towns RESTART IDENTITY CASCADE;");
+            await db.none("INSERT INTO towns(id, towns_name, start_town) VALUES(1, 'Cape Town', 'CA')");
+            await db.none("INSERT INTO towns(id, towns_name,start_town) VALUES(2, ' Queenstown','CH')");
+            await db.none("INSERT INTO towns(id, towns_name,start_town) VALUES(3, 'Paarl ','CJ')");
+    
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    });
+
     this.timeout(10000);
     it("should allow adding registration numbers", async function () {
+        let database = Query(db);
+
         await database.insertRegNum("CJ 123456", 3);
 
-        let result = await database.getRegistrations("All");
+        let result1 = await database.getRegistrations("All");
 
-        assert.equal(1, result.length);
+        assert.equal(1, result1.length);
     });
 
     it("should able to insert registration numbers for 3  available towns", async function () {
         this.timeout(10000);
         let database = Query(db);
 
-        await database.insertRegNum("CA 466 789", 1);
-        await database.insertRegNum("CF 121456", 2);
-        await database.insertRegNum("CJ 123-466", 3);
+        await database.insertRegNum("CA 466 789");
+        await database.insertRegNum("CA 121456");
+        await database.insertRegNum("CJ 123-466");
 
-        let result = await database.getRegistrations("All");
+        let result = await database.getRegistrations();
 
         assert.equal(3, result.length);
 
@@ -78,17 +106,6 @@ describe('the Registration Number App database function/Code', function () {
 
         assert.equal(0, result.length);
     });
-    it("should be able to return error message when the format is incorrect", async function () {
-        await database.insertRegNum("CAA 456 789");
-        await database.insertRegNum("CFF 123456");
-
-        let result = await database.getRegistrations("All");
-
-        assert.equal('Format not supported!' == 1, result.length);
-    });
-
-
-
     after(function () {
         db.$pool.end
     });
